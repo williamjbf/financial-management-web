2ded667a467aa3ddf82d50afdc14ae2d3798e68e
williamjbf <williamjbf@gmail.com>
2022-05-23 21:49:03 -0300

    updated project startup command

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd app && ./mvnw clean package -DskipTests && cp target/app-0.0.1-SNAPSHOT.jar ./app.jar && cd ..
 
-cd web && npm run build && cd ..
+cd web && npm install && npm run build && cd ..
 
 docker-compose up --build -d
