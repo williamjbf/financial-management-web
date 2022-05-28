5b8cbb52febae185da0087ad1425b0b60fb3e796
kauanny-cmd <kauannyvieira8@gmail.com>
2022-05-28 00:19:03 -0300

    reorganização do app

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
+import Header from './components/header/header';
 import './App.css';
+import Resume from './components/resume/resume';
+import Buttons from './components/buttonForm/buttonForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <Header />
+    <Resume/>
+    <Buttons/>
+    </>
   );
 }
 
