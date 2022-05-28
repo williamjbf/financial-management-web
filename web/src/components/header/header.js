517a7bfe9c3e5cbe93e57a7f37df8dfe814e0a8a
kauanny-cmd <kauannyvieira8@gmail.com>
2022-05-28 00:20:31 -0300

    header da aplicação

---
+++ web/src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import * as C from "./style";
+
+const Header = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.Title>Financial Management Web</C.Title>
+      </C.Header>
+    </C.Container>
+   
+  );
+};
+
+export default Header;
